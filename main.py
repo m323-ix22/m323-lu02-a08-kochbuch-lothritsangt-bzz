0771b618e0ebc1b5ae0f7f8bb52f1a240e723452
Choesang <lothritsangt@bzz.ch>
2024-09-09 13:55:17 +0200

    done

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-# Dein Code kommt hier hin
+"""kochbuch"""
+
+import json
+
+def load_recipe(json_string):
+    return json.loads(json_string)
+
+
+def adjust_recipe(recipe, new_servings):
+    factor = new_servings / recipe['servings']
+    adjusted_ingredients = {ingredient: amount * factor for ingredient, amount in recipe['ingredients'].items()}
+    return {
+        'title': recipe['title'],
+        'ingredients': adjusted_ingredients,
+        'servings': new_servings
+    }
+
 
 if __name__ == '__main__':
-    # Beispiel für die Datenstruktur eines Rezepts
     recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+
+    recipe = load_recipe(recipe_json)
+
+    new_servings = 2
+
+    adjusted_recipe = adjust_recipe(recipe, new_servings)
+
+    print(json.dumps(adjusted_recipe, indent=4))
