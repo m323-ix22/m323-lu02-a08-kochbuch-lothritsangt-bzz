e247c894158eafba1a6f73d63ff6d687047316d8
Choesang <lothritsangt@bzz.ch>
2024-09-09 13:57:17 +0200

    done

--- main.py
+++ main.py
@@ -2,22 +2,24 @@
 
 import json
 
+
 def load_recipe(json_string):
     return json.loads(json_string)
 
 
-def adjust_recipe(recipe, new_servings):
-    factor = new_servings / recipe['servings']
+def adjust_recipe(recipe1, new_servings1):
+    factor = new_servings1 / recipe1['servings']
     adjusted_ingredients = {ingredient: amount * factor for ingredient, amount in recipe['ingredients'].items()}
     return {
-        'title': recipe['title'],
+        'title': recipe1['title'],
         'ingredients': adjusted_ingredients,
-        'servings': new_servings
+        'servings': new_servings1
     }
 
 
 if __name__ == '__main__':
-    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
+    recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, ' \
+                  '"Minced Meat": 500}, "servings": 4}'
 
     recipe = load_recipe(recipe_json)
 
